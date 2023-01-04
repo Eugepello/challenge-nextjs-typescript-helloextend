29734183486a741475e188131a600da904bbd293
Eugenio Pello <eugepello@MacBook-Air-de-Eugenio.local>
2023-01-04 16:58:58 -0300

    Details

--- app/[breed]/page.tsx
+++ app/[breed]/page.tsx
@@ -10,7 +10,7 @@ export default async function BreedPage ({ params: { breed } }: Props) {
   const { message: images } = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/10`).then(res => res.json() as Promise<{message: string[]}>)
 
   return (
-    <section className='grid grid-cols-2 gap-4' style={{ gridTemplateColums: 'repeat(autofill, minmax(256px, 1fr))' }}>
+    <section className='grid grid-cols-2 gap-4' style={{ gridTemplateColumns: 'repeat(autofill, minmax(256px, 1fr))' }}>
       {
           images.map((image) => (
             <FavoritableImage key={image} src={image} />
